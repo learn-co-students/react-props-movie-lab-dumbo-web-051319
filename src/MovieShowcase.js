c92e8b5b5235f72a3f021b73f3b964cd7dcdaf70
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-07-15 15:39:30 -0400

    used props

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -3,9 +3,10 @@ import MovieCard from './card-components/MovieCard.js'
 import movieData from './data.js'
 
 export default class MovieShowcase extends Component {
-
   generateMovieCards = () => {
-    // map over your movieData array and return the correct 
+    return movieData.map((card, index) =>
+      <MovieCard key={index} title={card.title} IMDBRating={card.IMDBRating} genres={card.genres} poster={card.poster} />
+    )
   }
 
   render() {
